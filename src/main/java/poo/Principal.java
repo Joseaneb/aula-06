cb9fc9398558c729cd1ed1a5df1faa2c5b379b2a
Joseaneb <joseane_bortoli@hotmail.com>
2018-08-15 08:22:21 -0300

    Colocando outra saudação

--- src/main/java/poo/Principal.java
+++ src/main/java/poo/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("Olá Mundo");
         System.out.println("Teste1");
-        System.out.println("Teste2");
+        System.out.println("Olá POO");
+
     }
 }
