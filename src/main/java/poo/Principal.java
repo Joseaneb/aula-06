cec6236fdd569f5caf9476e10a2d2a54b703866f
Joseaneb <joseane_bortoli@hotmail.com>
2018-08-15 08:20:37 -0300

    Iniciando Repositório

--- src/main/java/poo/Principal.java
+++ src/main/java/poo/Principal.java
@@ -5,5 +5,6 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("Olá Mundo");
         System.out.println("Teste1");
+        System.out.println("Teste2");
     }
 }
